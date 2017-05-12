ce92b6360cb5cf55b2004b4ab306af0c7b825744
Jonathan Hensley <Jonathan_Hensley@teamhealth.com>
2017-05-12 16:05:32 -0400

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Jonathan Hensley on 5/12/17.
+//  Copyright © 2017 KNOXDEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
